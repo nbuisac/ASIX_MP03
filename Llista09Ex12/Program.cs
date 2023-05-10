99f5451ba438be0e8f3f892ac86660e1cde2feec
Narcis Buisac <nbuisac@institutmontilivi.cat>
2023-05-10 12:02:47 +0200

    10/05/2023

---
+++ Llista09Ex12/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Llista09Ex12;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string nomFitxer;
+        StreamReader f01;
+        StreamWriter f02;
+        char caracter;
+
+        Console.Write("Entra el nom del fitxer -> ");
+        nomFitxer = Console.ReadLine();
+
+        try {
+            f01 = new StreamReader(nomFitxer);
+            f02 = new StreamWriter(nomFitxer.Trim() + ".UPPER");
+            while (! f01.EndOfStream) {
+                caracter = (char)f01.Read();
+                f02.Write(Char.ToUpper(caracter));
+            }
+            f01.Close();
+            f02.Close();
+        }
+        catch (Exception e) {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
---
+++ Llista09Ex15/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Llista09Ex15;
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int CLAU = 2;
+
+        string nomFitxer;
+        StreamWriter f02;
+        StreamReader f01;
+        char c;
+
+        Console.Write("Entra el nom del fitxer -> ");
+        nomFitxer = Console.ReadLine();
+        try {
+            f01 = new StreamReader(nomFitxer);
+            f02 = new StreamWriter(nomFitxer.Trim() + ".Cesar");
+            while (! f01.EndOfStream) {
+                // Llegim caràcter
+                c = (char) f01.Read();
+                // Transformem el caràcter
+                if (c >= 'a' && c <= 'z') {
+                    c = (char)(((c - 'a' + CLAU) % ('z' - 'a' + 1)) + 'a');
+                } else if (c >= 'A' && c <= 'Z') {
+                    c = (char)(((c - 'A' + CLAU) % ('Z' - 'A' + 1)) + 'A');
+                }
+
+                // Mostrem el caràcter transformat
+                f02.Write(c);
+            }
+            f01.Close();
+            f02.Close();
+        }
+        catch (Exception e) {
+            Console.WriteLine(e.Message);
+        }
+
+    }
+}
